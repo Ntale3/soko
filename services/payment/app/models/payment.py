b745ab097d084b99dc1fe789e19eec1a8f9afadb
Ntale Samad <swamaduntale98@gmail.com>
2026-06-01 20:14:08 +0300

    feat(payment): add pesapal to PaymentMethodType enum

--- services/payment/app/models/payment.py
+++ services/payment/app/models/payment.py
@@ -18,6 +18,7 @@ class PaymentStatus(str, enum.Enum):
 
 
 class PaymentMethodType(str, enum.Enum):
+    pesapal          = "pesapal"
     mobile_money     = "mobile_money"
     cash_on_delivery = "cash_on_delivery"
     bank_transfer    = "bank_transfer"
